0d99a463ad90a8bc1d7c3bf2dffd9d46ee1681ba
jiunjiun <quietmes@gmail.com>
2015-12-13 18:57:50 +0800

    add rspec

--- spec/hasami_spec.rb
+++ spec/hasami_spec.rb
@@ -2,10 +2,41 @@ require 'spec_helper'
 
 describe Hasami do
   it 'has a version number' do
-    expect(Hasami::VERSION).not_to be nil
+    expect(HasamiSettings::VERSION).not_to be_nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  context 'methods' do
+    before do
+      @haha = Hasami.new
+    end
+
+    it 'flights' do
+      expect(@haha.flights).to be_truthy
+    end
+
+    it 'arrivals' do
+      arrivals = @haha.arrivals
+      expect(arrivals.first.kind).to eq 'Arrivals'
+    end
+
+    it 'departure' do
+      departure = @haha.departure
+      expect(departure.first.kind).to eq 'Departure'
+    end
+
+    it 'yesterday' do
+      yesterdays = @haha.yesterday
+      expect(yesterdays.first.datetime.to_date).to eq Date.today.prev_day
+    end
+
+    it 'today' do
+      todays = @haha.today
+      expect(todays.first.datetime.to_date).to eq Date.today
+    end
+
+    it 'tomorrow' do
+      tomorrow = @haha.tomorrow
+      expect(tomorrow.first.datetime.to_date).to eq Date.today.next_day
+    end
   end
 end
